bf17cc1ee30fdf526f1d436a07f09d4846242021
a-n-29 <anguyen2100@gmail.com>
2022-01-19 17:38:26 -0600

    Connecting SQLite DB in workspace kt file

--- src/main/kotlin/com/example/app/InventoryManagementWorkspace.kt
+++ src/main/kotlin/com/example/app/InventoryManagementWorkspace.kt
@@ -1,11 +1,17 @@
 package com.example.app
 
+import com.example.util.createTables
+import com.example.util.enableConsoleLogger
 import javafx.scene.control.TabPane
+import org.jetbrains.exposed.sql.Database
 import tornadofx.*
 
 class InventoryManagementWorkspace : Workspace("Inventory Management", NavigationMode.Tabs) {
     init {
         // initilize database etc.
+        enableConsoleLogger()
+        Database.connect("jdbc:sqlite:./app-inventory-manager.db", "org.sqlite.JDBC")
+        createTables()
 
         // pass in controller(s)
 
