a1b7d3110dc1944be96384c5c010cb3ec598bffb
a-n-29 <anguyen2100@gmail.com>
2022-01-19 01:16:46 -0600

    Setup Database Helper methods and logger in database.kt

--- src/main/kotlin/com/example/util/database.kt
+++ src/main/kotlin/com/example/util/database.kt
@@ -1,7 +1,34 @@
 package com.example.util
 
 
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import java.sql.Connection
 
 private var LOG_TO_CONSOLE: Boolean = false
 
+fun newTransaction(): Transaction = TransactionManager.currentOrNew(Connection.TRANSACTION_SERIALIZABLE).apply {
+    if (LOG_TO_CONSOLE) addLogger(StdOutSqlLogger)
+}
+
+fun enableConsoleLogger(){
+    LOG_TO_CONSOLE = true
+}
+
+fun createTables() {
+    with(newTransaction()){
+        SchemaUtils.create(IngredientsEntryTbl)
+    }
+}
+
+fun <T> execute(command: () -> T) : T {
+    with(newTransaction()) {
+        return command().apply {
+            commit()
+            close()
+        }
+    }
+}
