6b83c3982baa3182e53d7367266a9eaef4320e91
a-n-29 <anguyen2100@gmail.com>
2022-01-19 00:41:49 -0600

    Setup TornadoFX Workspace and override stage, set width and height

--- src/main/kotlin/com/example/app/MyApp.kt
+++ src/main/kotlin/com/example/app/MyApp.kt
@@ -1,6 +1,17 @@
 package com.example.app
 
 import com.example.view.MainView
+import javafx.stage.Stage
 import tornadofx.App
 
-class MyApp: App(InventoryManagementWorkspace::class, Styles::class)
+class MyApp: App(InventoryManagementWorkspace::class, Styles::class){
+
+    override fun start(stage: Stage) {
+        with(stage){
+            width = 1200.0
+            height = 600.0
+        }
+
+        super.start(stage)
+    }
+}
