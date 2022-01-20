7f54659c0d3b66be0ddd6b983a131ead7a65eb6e
a-n-29 <anguyen2100@gmail.com>
2022-01-19 18:02:29 -0600

    Saving an Item to DB
    right now, manual in Workspace kt

--- src/main/kotlin/com/example/app/InventoryManagementWorkspace.kt
+++ src/main/kotlin/com/example/app/InventoryManagementWorkspace.kt
@@ -1,10 +1,15 @@
 package com.example.app
 
+import com.example.model.IngredientsEntryTbl
 import com.example.util.createTables
 import com.example.util.enableConsoleLogger
+import com.example.util.execute
+import com.example.util.toDate
 import javafx.scene.control.TabPane
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.insert
 import tornadofx.*
+import java.time.LocalDate
 
 class InventoryManagementWorkspace : Workspace("Inventory Management", NavigationMode.Tabs) {
     init {
@@ -12,6 +17,18 @@ class InventoryManagementWorkspace : Workspace("Inventory Management", Navigatio
         enableConsoleLogger()
         Database.connect("jdbc:sqlite:./app-inventory-manager.db", "org.sqlite.JDBC")
         createTables()
+
+
+        val newEntry = execute {
+            IngredientsEntryTbl.insert {
+                it[itemName]="carrot"
+                it[subtype]="soup base"
+                it[storageLocation]="drawer left of fridge"
+                it[quantity]=2
+                it[datePurchased]= LocalDate.now().toDate()
+                it[additionalInformation]="use on Tuesday"
+            }
+        }
 
         // pass in controller(s)
 
